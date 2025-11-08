2b0da22ec07c46af26cea5c7f7e7de744389603c
IEVN1003-22001393 <0831alejandro@gmail.com>
2025-11-07 18:48:06 -0600

    Pizza - 07/11/25

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from flask import make_response, jsonify
+import json
 import forms
 import math
 
@@ -8,19 +10,120 @@ app = Flask(__name__)
 def home():
     return "Hello world"
 
+@app.route('/pizza', methods=['GET','POST'])
+def pizza():
+    mostrarVen = False
+# ---------------------------------------- Cargado del formulario y cookies
+    form = forms.PizzasForm(request.form)
+    pedido_actual_str = request.cookies.get('pedido_actual', '[]')
+    pedido_actual = json.loads(pedido_actual_str)
+   
+    ventas_str = request.cookies.get('ventas_dia', '[]')
+    ventas = json.loads(ventas_str)
+   
+    ventasAgrupadas = {}
+    total_dia = 0
+    for venta in ventas:
+        nombre = venta.get('nombre')
+        total = venta.get('total')
+        total_dia += total
+        ventasAgrupadas[nombre] = ventasAgrupadas.get(nombre, 0) + total
+   
+# ---------------------------------------- Formulario
+    if request.method == 'POST':
+     
+        action = request.form.get("action")
+        if action == 'agregar':
+            precios_tamanio = {'chica': 40, 'mediana': 80, 'grande': 120}
+           
+            tamanio = request.form.get('Tamanio')
+            ingredientesLista = request.form.getlist('Ingredientes')
+            num_pizzas = form.NumPizzas.data
+           
+            if tamanio and num_pizzas:
+                costTmnio = precios_tamanio.get(tamanio)
+                costIngre = len(ingredientesLista) * 10
+                subtotal = (costTmnio + costIngre) * num_pizzas
+                pizzaOrdenada = {"tamanio": tamanio, "ingredientes": ingredientesLista, "num_pizzas": num_pizzas, "subtotal": subtotal}
+                pedido_actual.append(pizzaOrdenada)
+        elif action == 'terminar':
+            total_pedido = 0
+            for item in pedido_actual:
+                total_pedido += item.get('subtotal')
+            newVenta = {"nombre": form.Nombre.data, "direccion": form.Direccion.data, "telefono": form.Telefono.data, "fecha": form.Fecha.data, "total": total_pedido}
+            ventas.append(newVenta)
+            ventasAgrupadas = {}
+            total_dia = 0
+            for venta in ventas:
+                nombre = venta.get('nombre')
+                total = venta.get('total')
+                total_dia += total
+                ventasAgrupadas[nombre] = ventasAgrupadas.get(nombre, 0) + total
+           
+            pedido_actual = []
+            form = forms.PizzasForm()
+        elif action == 'quitar_ultimo':
+            if pedido_actual:
+                pedido_actual.pop()
+ 
+        elif action == 'mostrarVen':
+            mostrarVen = True
+            
+        elif action == 'borrarVen':
+            ventas = []
+            ventasAgrupadas = {}
+            total_dia = 0
+            mostrarVen = False
+   
+    response = make_response(render_template('pizza.html', form=form, pedido=pedido_actual, ventas_agrupadas=ventasAgrupadas, total_dia=total_dia, mostrarVen=mostrarVen))
+   
+    response.set_cookie('pedido_actual', json.dumps(pedido_actual))
+    response.set_cookie('ventas_dia', json.dumps(ventas))
+ 
+    return response
+
 @app.route("/Alumnos", methods=['GET', 'POST'])
 def alumnos():
     mat=0
     nom="" 
     ape=""
     em=""
+    estudiantes=[]
+    datos={}
+
     alumnos_clase=forms.UserForm(request.form)
     if request.method=='POST' and alumnos_clase.validate():
         mat=alumnos_clase.matricula.data
         nom=alumnos_clase.nombre.data
         ape=alumnos_clase.apellido.data
         em=alumnos_clase.correo.data
-    return render_template('alumnos.html', form=alumnos_clase, mat=mat, nom=nom, ape=ape, em=em)
+        datos={"matricula":mat, "nombre":nom, "apellido":ape, "correo":em}
+
+        datos_str=request.cookies.get('estudiante')
+        if not datos_str:
+            return"No hay cookies"
+        tem=json.loads(datos_str)
+        estudiantes=tem
+        estudiantes=json.loads(datos_str)
+        print(type(estudiantes))
+        estudiantes.append(datos)
+ 
+    response=make_response(render_template('alumnos.html', form=alumnos_clase,mat=mat,nom=nom,ape=ape,em=em))
+   
+    response.set_cookie('estudiante', json.dumps(estudiantes))
+    return response
+ 
+
+@app.route("/get_cookie")
+def get_cookie():
+    datos_str=request.cookies.get('estudiante')
+    if not datos_str:
+        return"No hay cookies"
+    datos=json.loads(datos_str)
+    
+    return jsonify(datos)
+
+
 
 @app.route('/figuras', methods=['GET', 'POST'])
 def figuras():
--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from wtforms import Form 
 from wtforms import StringField, FloatField, EmailField, PasswordField, IntegerField, RadioField
 from wtforms import validators
+import datetime
 
 class UserForm(Form):
     matricula = IntegerField('Matricula', [validators.DataRequired(message="La matricula es obligatoria")])
@@ -15,4 +16,19 @@ class FigForm(Form):
     figura = RadioField('Selecciona la figura', choices=[('triangulo', 'Triangulo'), ('rectangulo', 'Rectangulo'), ('circulo', 'Circulo'), ('pentagono', 'Pentagono')], validators=[validators.DataRequired(message="Seleccione una figura.")])
     val1 = FloatField('Base / Radio / Lado', [validators.DataRequired(message="El campo es requerido.")])
     val2 = FloatField('Altura')
+
+class PizzasForm(Form):
+    Nombre = StringField("Nombre",
+            [validators.DataRequired(message="El nombre es obligatorio")])
+    Direccion = StringField("Dirección",
+            [validators.DataRequired(message="La dirección es obligatoria")])
+    Telefono = StringField("Teléfono",
+            [validators.DataRequired(message="El teléfono es obligatorio")])
+ 
+    Fecha = StringField("Fecha",
+            default=datetime.date.today().strftime('%Y-%m-%d'))
+ 
+    NumPizzas = IntegerField("Numero de Pizzas",
+            [validators.Optional(), validators.NumberRange(min=1, message="Debe ser al menos 1")],
+            default=1)
    
